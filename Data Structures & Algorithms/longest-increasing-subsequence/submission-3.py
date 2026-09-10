a911948e259c47cadf6897f929abad6932f80abb
Arjun Tyagi <arjun25@illinois.edu>
2026-09-09 22:26:29 -0500

    Add: longest-increasing-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # memo = {}
+        # def rec(curr_index, prev_index):
+        #     if curr_index == len(nums):
+        #         return 0
+
+        #     if (curr_index, prev_index) in memo:
+        #         return memo[(curr_index, prev_index)]
+
+        #     res = 0
+        #     if prev_index == -1 or nums[prev_index] < nums[curr_index]:
+        #         res = max(res, 1 + rec(curr_index+1, curr_index))
+
+        #     res = max(res, rec(curr_index+1, prev_index))
+        #     memo[(curr_index, prev_index)] = res
+
+        #     return res
+
+        # return rec(0, -1)
+
+        dp = [1] * len(nums)
+
+        for curr_index in range(len(nums)-1, -1, -1):
+            for i in range(curr_index + 1, len(nums)):
+                if nums[i] > nums[curr_index]:
+                    dp[curr_index] = max(dp[curr_index], 1 + dp[i])
+
+        return max(dp)
